0164cc98900f9ada6d501a3fe88c012d1aefe122
daniel-parra16 <danielestebanparraruiz22@gmail.com>
2025-12-10 15:28:29 -0500

    Correccion del enrurtamiento desde app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
-
 import App from './App.jsx';
-import Layout from './layout/layout.jsx';
-import Forgot from './pages/Forgot.jsx';
-import Home from './pages/Home.jsx';
-import Login from './pages/Login.jsx';
-import Prueba from './pages/Prueba.jsx';
-import Register from './pages/Register.jsx';
-import ReservasD from './pages/ReservasD.jsx';
-import Contactos from './pages/Contactos.jsx';
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <Routes>
-      {/*RUTA PRINCIPAL*/}
-      <Route path="/" element={<App> </App>} />
-
-      {/*RUTAS SECUNDARIAS*/}
-      <Route element={<Layout/>}>
-        <Route path="/prueba" element={<Prueba />} />
-        <Route path="/home" element={<Home />} />
-        <Route path='/contactos' element={<Contactos />} />
-      </Route>
-
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/forgot" element={<Forgot />} />
-      <Route path='/reservas-deportivas' element={<ReservasD />} />
-      
-    </Routes>
-    </BrowserRouter>
-    
+    <App />
   </StrictMode>
 )
