ddb87b9ead06682f1c1b7e0a3bde621257b98197
andres2006 <chaparrofelipe60@gmail.com>
2026-03-11 23:40:11 -0500

    Cambios en Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,6 +8,7 @@ import click from '../assets/click.png';
 import logo from '../assets/LOGO.png'
 import { useNavigate } from "react-router-dom";
 
+
 function Register() {
   const [verPass, setVerPass] = useState(false);
   const [verConfirm, setVerConfirm] = useState(false);
@@ -16,74 +17,86 @@ function Register() {
   return (
     <Row className="register-content">
       <Col>
-      <div className="left-panel-register"> 
-        <Form className="login-form px-3">
-          <Row>
-            <header className="site-header">
-              <img src={logo} alt="logo" className="site-logo" />
-            </header>
-          </Row>
+        <div className="left-panel-register">
+          <Form className="login-form px-3">
+            <Row>
+              <header className="site-header">
+                <img src={logo} alt="logo" className="site-logo" />
+              </header>
+            </Row>
 
-          <Row className="justify-content-center align-items-center form-container mx-3">
-            <div className="page-heading">
-              <h1 className="title-register">Crea tu cuenta</h1>
-              <p className="subtitle-register"> Registrate para acceder a tu reserva</p>
-            </div>
-
-            <registro-content className="form-register">
-
-              <Form.Group className="mb-2" controlId="formBasicName">
-                <Form.Control className="inp" type="text" placeholder="Nombre Completo" />
-              </Form.Group>
-
-              <Form.Group className="mb-2" controlId="formBasicEmail">
-                <Form.Control className="inp" type="email" placeholder="usuario.nombre@gmail.com" />
-              </Form.Group>
-              {/* CONTRASEÑA */}
-              <div className="input-wrapper">
-                <Form.Control
-                  className="inp"
-                  type={verPass ? "text" : "password"}
-                  placeholder="Contraseña"
-                />
-                <button
-                  type="button"
-                  className="btn-eye"
-                  onClick={() => setVerPass(!verPass)}
-                >
-                  {verPass ? "🙈" : "👁️"}
-                </button>
+            <Row className="justify-content-center align-items-center form-container mx-3">
+              <div className="page-heading">
+                <h1 className="title-register">Crea tu cuenta</h1>
+                <p className="subtitle-register"> Registrate para acceder a tu reserva</p>
               </div>
 
-              {/* CONFIRMAR CONTRASEÑA */}
-              <div className="input-wrapper">
-                <Form.Control
-                  className="inp"
-                  type={verConfirm ? "text" : "password"}
-                  placeholder="Confirmar Contraseña"
-                />
-                <button
-                  type="button"
-                  className="btn-eye"
-                  onClick={() => setVerConfirm(!verConfirm)}
-                >
-                  {verConfirm ? "🙈" : "👁️"}
-                </button>
-              </div>
+              <registro-content className="form-register">
 
-            </registro-content>
-            <button type="button" className="btn-register" onClick={() => navigate("/login")}>
-              REGISTRARME
-            </button>
+                <Form.Group className="mb-2" controlId="formBasicName">
+                  <Form.Control className="inp" type="text" placeholder="Nombre Completo" />
+                </Form.Group>
 
-          </Row>
-        </Form>
-      </div>
+                <Form.Group className="mb-2" controlId="formBasicName">
+                  <Form.Control className="inp" type="text" placeholder="Apellido completo" />
+                </Form.Group>
+
+                <Form.Select className="inp" controlId="formBasicDocumentType">
+                  <option value="">Tipo de documento</option>
+                  <option value="CC">Cédula de ciudadanía (CC)</option>
+                  <option value="TI">Tarjeta de identidad (TI)</option>
+                  <option value="CE">Cédula de extranjería (CE)</option>
+                  <option value="PP">Pasaporte (PP)</option>
+                </Form.Select>
+
+                <Form.Group className="mb-2" controlId="formBasicEmail">
+                  <Form.Control className="inp" type="email" placeholder="usuario.nombre@gmail.com" />
+                </Form.Group>
+                {/* CONTRASEÑA */}
+                <div className="input-wrapper">
+                  <Form.Control
+                    className="inp"
+                    type={verPass ? "text" : "password"}
+                    placeholder="Contraseña"
+                  />
+                  <button
+                    type="button"
+                    className="btn-eye"
+                    onClick={() => setVerPass(!verPass)}
+                  >
+                    {verPass ? "🙈" : "👁️"}
+                  </button>
+                </div>
+
+                {/* CONFIRMAR CONTRASEÑA */}
+                <div className="input-wrapper">
+                  <Form.Control
+                    className="inp"
+                    type={verConfirm ? "text" : "password"}
+                    placeholder="Confirmar Contraseña"
+                  />
+                  <button
+                    type="button"
+                    className="btn-eye"
+                    onClick={() => setVerConfirm(!verConfirm)}
+                  >
+                    {verConfirm ? "🙈" : "👁️"}
+                  </button>
+                </div>
+
+              </registro-content>
+              <button type="button" className="btn-register" onClick={() => navigate("/login")}>
+                REGISTRARME
+              </button>
+
+            </Row>
+          </Form>
+        </div>
       </Col>
       <Col className="right-panel-register">
-      <div className="logo-container-register ">
-        <img src={click} alt="click" className="click-img" />
-      </div>
+        <div className="logo-container-register ">
+          <img src={click} alt="click" className="click-img" />
+        </div>
       </Col>
     </Row>
   );
